ad86658a904e6d3431f62286b8cb6dc4dc387d35
Gabriel Matheus <133973057+GabrielMatheus1@users.noreply.github.com>
2025-03-13 16:55:12 -0300

    Delete Login-Cadastro directory

--- Login-Cadastro/cadastro.js
+++
@@ -1,6 +0,0 @@
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-const senhaConfirm = document.getElementById("senha-confirm");
-
-addEventListener("")
